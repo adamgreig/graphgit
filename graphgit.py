3fa2804da9f431b551dbb0ada2edfccd8ffcee9e
Adam Greig <adam@adamgreig.com>
2012-05-31 00:25:40 +0100

    use dot for layout, points for commits, new colors

--- graphgit.py
+++ graphgit.py
@@ -7,29 +7,29 @@ import os.path
 G = pygraphviz.AGraph(strict=False, directed=True)
 G.graph_attr['rankdir'] = 'RL'
 G.graph_attr['overlap'] = 'false'
-G.node_attr['colorscheme'] = 'set19'
-G.edge_attr['colorscheme'] = 'set19'
-G.edge_attr['dir'] = 'back'
+G.node_attr['colorscheme'] = 'paired9'
+G.node_attr['shape'] = 'point'
+G.edge_attr['colorscheme'] = 'paired9'
+G.edge_attr['dir'] = 'none'
 
-commits = {}
 names = set()
 
 def name_to_int(name):
     return (int(hashlib.md5(name).hexdigest(), 16) % 9) + 1
 
 def process_commit(c):
-    global G
+    global G, names
+
     while not G.has_node(c.hexsha[0:5]):
         names.add(c.author.name)
         G.add_node(c.hexsha[0:5], color=name_to_int(c.author.name))
-        commits[c.hexsha[0:5]] = c
         for p in c.parents:
             process_commit(p)
             G.add_edge(c.hexsha[0:5], p.hexsha[0:5],
                     color=name_to_int(c.author.name))
 
 def main():
-    global G
+    global G, names
 
     if len(sys.argv) == 1:
         print "Usage: {0} <path to git repo>".format(sys.argv[0])
@@ -41,7 +41,6 @@ def main():
         path = path[:-1]
 
     title = os.path.basename(path)
-    G.graph_attr['label'] = title
 
     try:    
         repo = git.Repo(path)
@@ -56,12 +55,12 @@ def main():
         G.add_edge(head.name, head.commit.hexsha[0:5], dir='none')
 
     for name in names:
-        G.add_node(name, color=name_to_int(name))
+        G.add_node(name, color=name_to_int(name), shape='box')
 
     with open(title + ".dot", "w") as f:
         f.write(str(G))
 
-    G.layout()
+    G.layout("dot")
     G.draw(title + ".png")
 
 if __name__ == "__main__":
